91f570b0c453501ffb3da09723fd3a3095b67273
Eldamorh <75213027+Eldamorh@users.noreply.github.com>
2021-01-29 18:26:58 +0200

    Update Main.java

--- src/com/eldus/Main.java
+++ src/com/eldus/Main.java
@@ -12,7 +12,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-//        //Задание 1
+        //Задание 1
         LocalDateTime startOfTheLectionInMonday = LocalDateTime.of(2021, 1, 25, 20, 0);
         LocalDateTime endOfTheLectionInThursday = LocalDateTime.of(2021, 1, 28, 22, 0);
 
